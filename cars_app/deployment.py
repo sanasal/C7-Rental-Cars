c5b7425155edd085425f8c2ed4ad2048ca6e99ad
sanasalhdar@gmail.com <sanasalhdar@gmail.com>
2024-09-01 20:04:05 +0200

    add STATICFILES_STORAGE to deploymn=ent file

--- cars_app/deployment.py
+++ cars_app/deployment.py
@@ -45,7 +45,7 @@ MIDDLEWARE = [
 ]
 
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 
 STATIC_URL = '/static/'
