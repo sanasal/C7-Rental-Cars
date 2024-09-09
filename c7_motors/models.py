f2d33674d126e32e07e94e871a3283920c30f782
sanasalhdar@gmail.com <sanasalhdar@gmail.com>
2024-09-09 20:06:44 +0200

    edit economy reservation model

--- c7_motors/models.py
+++ c7_motors/models.py
@@ -139,16 +139,12 @@ class Cart(models.Model):
 
 class economy_reservation(models.Model):
     car = models.ForeignKey(economy_car, on_delete=models.CASCADE , related_name='items')
+    price = models.ForeignKey(economy_car, on_delete=models.CASCADE , related_name='price_items')
+    insurnce = models.ForeignKey(economy_car, on_delete=models.CASCADE , related_name='insurnce_items')
     cart = models.ForeignKey(Cart, on_delete = models.CASCADE , related_name='cartitems')
 
     def __str__(self):
        return self.car.name
-
-    def __str__(self):
-       return self.car.price
-
-    def __str__(self):
-       return self.car.insurnce
 
 class luxury_reservation(models.Model):
     lux_car = models.ForeignKey(luxury_car, on_delete = models.CASCADE , related_name='items2' , null=True)
